ffdc931fbaa892379f15d24fc711d518793cff32
Jesús Andrés Hincapié <jahlon@gmail.com>
2018-12-05 17:21:21 -0500

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev' ) {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = 'mongodb://cafe-user:tripichin22@ds159497.mlab.com:59497/cafe';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
