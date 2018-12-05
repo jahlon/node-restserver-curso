983481fd393cd7e8f8eade83b927a12f04dfdbdf
Jesús Andrés Hincapié <jahlon@gmail.com>
2018-12-05 17:05:09 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 // Configuración del puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Base de datos
+let urlDB;
+if( process.env.NODE_ENV === 'dev' ) {
+  urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+  urlDB = 'mongodb://cafe-user:tripichin22@ds159497.mlab.com:59497/cafe';
+}
+
+process.env.URL_DB = urlDB;
+
